6e28e56856a595ab2a356798e77ea0a3437ea011
Marc Eidelhoch <meidelhoch@gmail.com>
2025-04-16 12:21:40 -0500

    working on laoding data

--- ProductionCode/data.py
+++ ProductionCode/data.py
@@ -1,5 +1,26 @@
+import os
 
 def get_data(filename):
-	with open(filename, "r", encoding="utf-8") as f:
-		data = f.readlines()
-	return data
+    data_folder = os.path.join(os.path.dirname(__file__), '../Data')
+    file_path = os.path.join(data_folder, filename)
+    
+    with open(file_path, "r", encoding="utf-8") as f:
+        data = f.readlines()
+    
+    cleaned_data = [clean_line(line) for line in data]
+
+
+    return cleaned_data
+
+def clean_line(line):
+    """
+    Cleans a line of data by removing leading and trailing whitespace.
+    """
+    print(line[3])
+    return line.strip()
+
+if __name__ == "__main__":
+    data = get_data("test_data.csv")
+    print(data[3])
+
+
