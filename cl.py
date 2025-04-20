998ae281c4d4f84396d065ba287957e6c038b2e1
Marc Eidelhoch <meidelhoch@gmail.com>
2025-04-20 18:20:26 -0500

    added notes for cl.py

--- cl.py
+++ cl.py
@@ -1,5 +1,9 @@
 '''
 The eventual location for the command line interface (CLI) for the project.
 This will be the entry point for the project when run from the command line.
+
+Needs one function for the command line input overall (so basically if statements for the first part of argv (either --search or --random or --help))
+Needs one function for search and one function for random and one function for help which are called by the preceding function
+Each of those functions can call from functions in ProductionCode
 '''
 
