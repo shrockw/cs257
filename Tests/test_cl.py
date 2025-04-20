34171fd514c140ac895f0fbcb80f531dbf8eee4c
Marc Eidelhoch <meidelhoch@gmail.com>
2025-04-20 17:05:03 -0500

    added functions to get data

--- Tests/test_cl.py
+++ Tests/test_cl.py
@@ -1,20 +1,31 @@
+'''
+Contains tests for the command line interface of the program.
+'''
+
 import unittest
 import os
-from ProductionCode.data import get_data
+import ast
+from ProductionCode.data import get_data, get_filepath
 
 
 class TestData(unittest.TestCase):
+    '''
+    Extends the unittest.TestCase class to test the get_data function.
+    '''
+
     def test_data(self):
         '''
         Tests for the data.
         '''
 
-        data_folder = os.path.join(os.path.dirname(__file__), '')
-        file_path = os.path.join(data_folder, "loaded_data_test.txt")
+        expected_data_file_path = get_filepath("loaded_data_test.txt", "Tests")
 
-        with open(file_path, "r", encoding="utf-8") as f:
-            data = f.readlines()
-        self.assertEqual(get_data("test_data.csv"), data, "Should be the same")
+        with open(expected_data_file_path, "r", encoding="utf-8") as f:
+            expected_data = f.readlines()[0]
+
+        self.assertEqual(get_data("test_data.csv", "Data"),
+                         ast.literal_eval(expected_data), "Should be the same")
+
 
 if __name__ == "__main__":
     unittest.main()
