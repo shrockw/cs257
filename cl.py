1913f1a55f4e11578fe5628d32e02850f533e05d
Anika Rajbhandary <126826820+rajbhandarya@users.noreply.github.com>
2025-04-21 17:13:08 -0500

    Update cl.py

--- cl.py
+++ cl.py
@@ -32,10 +32,14 @@ def main():
 
 
 def usage_statement():
-    ''' Display the usage statement for the command line interface. '''
+    ''' Display the usage statements for the command line interface. '''
     print("Usage: python cl.py --search --include_ingredients <ingredients> --omit_ingredients <ingredients>")
+    print("<ingredients> should be a comma-separated list of ingredients enclosed in quotes.")
     print("or python cl.py --random <number>")
     print("or python cl.py --help")
+    print("--search or --s: Search for a specific recipe.")
+    print("--random or --r: Get a random recipe.")
+    print("--help or --h: Display help message.")
 
 
 def search():
