8e81ed3ce10a8f5aba3c3b964a10be48aea4623a
Marc Eidelhoch <meidelhoch@gmail.com>
2025-04-21 19:48:43 -0500

    more formatting

--- Tests/test_cl.py
+++ Tests/test_cl.py
@@ -102,8 +102,26 @@ class TestRecipeSearch(unittest.TestCase):
         self.assertEqual(find_recipes(test_recipes, ["Ingredient1", "Ingredient3"], [
         ]), expected_recipes, "Should be the same")
 
-        expected_recipes = [['0', 'Title1', 'Instructions for Title1', ['Ingredient1', 'Ingredient2']], ['1', 'Title2', 'Instructions for Title2', ['Ingredient3', 'Ingredient4']], ['2', 'Title3', 'Instructions for Title3', ['Ingredient5', 'Ingredient6']], ['3', 'Title4', 'Instructions for Title4', ['Ingredient7', 'Ingredient8']], ['4', 'Title5', 'Instructions for Title5', ['Ingredient9', 'Ingredienta']], [
-            '5', 'Title6', 'Instructions for Title6', ['Ingredientb', 'Ingredientc']], ['6', 'Title7', 'Instructions for Title7', ['Ingredientd', 'Ingrediente']], ['7', 'Title8', 'Instructions for Title8', ['Ingredientf', 'Ingredientg', 'Ingredient3']], ['8', 'Title9', 'Instructions for Title9', ['Ingredienth', 'Ingredienti', 'Ingredient7', 'Ingredient1']], ['9', 'Title10', 'Instructions for Title10', ['Ingredient4', 'Ingredient8', 'Ingrediente']]]
+        expected_recipes = [['0', 'Title1', 'Instructions for Title1',
+                             ['Ingredient1', 'Ingredient2']],
+                            ['1', 'Title2', 'Instructions for Title2',
+                             ['Ingredient3', 'Ingredient4']],
+                            ['2', 'Title3', 'Instructions for Title3',
+                             ['Ingredient5', 'Ingredient6']],
+                            ['3', 'Title4', 'Instructions for Title4',
+                             ['Ingredient7', 'Ingredient8']],
+                            ['4', 'Title5', 'Instructions for Title5',
+                             ['Ingredient9', 'Ingredienta']],
+                            ['5', 'Title6', 'Instructions for Title6',
+                             ['Ingredientb', 'Ingredientc']],
+                            ['6', 'Title7', 'Instructions for Title7',
+                             ['Ingredientd', 'Ingrediente']],
+                            ['7', 'Title8', 'Instructions for Title8',
+                             ['Ingredientf', 'Ingredientg', 'Ingredient3']],
+                            ['8', 'Title9', 'Instructions for Title9',
+                             ['Ingredienth', 'Ingredienti', 'Ingredient7', 'Ingredient1']],
+                            ['9', 'Title10', 'Instructions for Title10',
+                             ['Ingredient4', 'Ingredient8', 'Ingrediente']]]
         test_recipes = get_data("test_data.csv", "Data")
         self.assertEqual(find_recipes(test_recipes, ["Ingredient"], [
         ]), expected_recipes, "Should be the same")
@@ -113,14 +131,42 @@ class TestRecipeSearch(unittest.TestCase):
         Tests the recipe search function.
         '''
 
-        expected_recipes = [['0', 'Title1', 'Instructions for Title1', ['Ingredient1', 'Ingredient2']], ['1', 'Title2', 'Instructions for Title2', ['Ingredient3', 'Ingredient4']], ['3', 'Title4', 'Instructions for Title4', ['Ingredient7', 'Ingredient8']], ['4', 'Title5', 'Instructions for Title5', ['Ingredient9', 'Ingredienta']], ['5', 'Title6', 'Instructions for Title6', ['Ingredientb', 'Ingredientc']], [
-            '6', 'Title7', 'Instructions for Title7', ['Ingredientd', 'Ingrediente']], ['7', 'Title8', 'Instructions for Title8', ['Ingredientf', 'Ingredientg', 'Ingredient3']], ['8', 'Title9', 'Instructions for Title9', ['Ingredienth', 'Ingredienti', 'Ingredient7', 'Ingredient1']], ['9', 'Title10', 'Instructions for Title10', ['Ingredient4', 'Ingredient8', 'Ingrediente']]]
+        expected_recipes = [['0', 'Title1', 'Instructions for Title1',
+                             ['Ingredient1', 'Ingredient2']],
+                            ['1', 'Title2', 'Instructions for Title2',
+                             ['Ingredient3', 'Ingredient4']],
+                            ['3', 'Title4', 'Instructions for Title4',
+                             ['Ingredient7', 'Ingredient8']],
+                            ['4', 'Title5', 'Instructions for Title5',
+                             ['Ingredient9', 'Ingredienta']],
+                            ['5', 'Title6', 'Instructions for Title6',
+                             ['Ingredientb', 'Ingredientc']],
+                            ['6', 'Title7', 'Instructions for Title7',
+                             ['Ingredientd', 'Ingrediente']],
+                            ['7', 'Title8', 'Instructions for Title8',
+                             ['Ingredientf', 'Ingredientg', 'Ingredient3']],
+                            ['8', 'Title9', 'Instructions for Title9',
+                             ['Ingredienth', 'Ingredienti', 'Ingredient7', 'Ingredient1']],
+                            ['9', 'Title10', 'Instructions for Title10',
+                             ['Ingredient4', 'Ingredient8', 'Ingrediente']]]
         test_recipes = get_data("test_data.csv", "Data")
         self.assertEqual(find_recipes(test_recipes, [], [
                          "Ingredient5"]), expected_recipes, "Should be the same")
 
-        expected_recipes = [['0', 'Title1', 'Instructions for Title1', ['Ingredient1', 'Ingredient2']], ['1', 'Title2', 'Instructions for Title2', ['Ingredient3', 'Ingredient4']], ['2', 'Title3', 'Instructions for Title3', ['Ingredient5', 'Ingredient6']], ['3', 'Title4', 'Instructions for Title4', [
-            'Ingredient7', 'Ingredient8']], ['5', 'Title6', 'Instructions for Title6', ['Ingredientb', 'Ingredientc']], ['7', 'Title8', 'Instructions for Title8', ['Ingredientf', 'Ingredientg', 'Ingredient3']], ['8', 'Title9', 'Instructions for Title9', ['Ingredienth', 'Ingredienti', 'Ingredient7', 'Ingredient1']]]
+        expected_recipes = [['0', 'Title1', 'Instructions for Title1',
+                             ['Ingredient1', 'Ingredient2']],
+                            ['1', 'Title2', 'Instructions for Title2',
+                             ['Ingredient3', 'Ingredient4']],
+                            ['2', 'Title3', 'Instructions for Title3',
+                             ['Ingredient5', 'Ingredient6']],
+                            ['3', 'Title4', 'Instructions for Title4',
+                             ['Ingredient7', 'Ingredient8']],
+                            ['5', 'Title6', 'Instructions for Title6',
+                             ['Ingredientb', 'Ingredientc']],
+                            ['7', 'Title8', 'Instructions for Title8',
+                             ['Ingredientf', 'Ingredientg', 'Ingredient3']],
+                            ['8', 'Title9', 'Instructions for Title9',
+                             ['Ingredienth', 'Ingredienti', 'Ingredient7', 'Ingredient1']]]
         test_recipes = get_data("test_data.csv", "Data")
         self.assertEqual(find_recipes(test_recipes, [], [
                          "Ingrediente", "Ingredient9"]), expected_recipes, "Should be the same")
@@ -144,24 +190,24 @@ class TestRecipeSearch(unittest.TestCase):
                          "Ingredient11"]), expected_recipes, "Should be the same")
 
         expected_recipes = [['0', 'Title1', 'Instructions for Title1',
-                             ['Ingredient1', 'Ingredient2']], 
-                            ['1', 'Title2', 'Instructions for Title2', 
-                             ['Ingredient3', 'Ingredient4']], 
-                            ['2', 'Title3', 'Instructions for Title3', 
-                             ['Ingredient5', 'Ingredient6']], 
-                            ['3', 'Title4', 'Instructions for Title4', 
-                             ['Ingredient7', 'Ingredient8']], 
-                            ['4', 'Title5', 'Instructions for Title5', 
-                             ['Ingredient9', 'Ingredienta']], 
-                            ['5', 'Title6', 'Instructions for Title6', 
-                             ['Ingredientb', 'Ingredientc']], 
-                            ['6', 'Title7', 'Instructions for Title7', 
-                             ['Ingredientd', 'Ingrediente']], 
-                            ['7', 'Title8', 'Instructions for Title8', 
-                             ['Ingredientf', 'Ingredientg', 'Ingredient3']], 
-                            ['8', 'Title9', 'Instructions for Title9', 
-                             ['Ingredienth', 'Ingredienti', 'Ingredient7', 'Ingredient1']], 
-                             ['9', 'Title10', 'Instructions for Title10', 
+                             ['Ingredient1', 'Ingredient2']],
+                            ['1', 'Title2', 'Instructions for Title2',
+                             ['Ingredient3', 'Ingredient4']],
+                            ['2', 'Title3', 'Instructions for Title3',
+                             ['Ingredient5', 'Ingredient6']],
+                            ['3', 'Title4', 'Instructions for Title4',
+                             ['Ingredient7', 'Ingredient8']],
+                            ['4', 'Title5', 'Instructions for Title5',
+                             ['Ingredient9', 'Ingredienta']],
+                            ['5', 'Title6', 'Instructions for Title6',
+                             ['Ingredientb', 'Ingredientc']],
+                            ['6', 'Title7', 'Instructions for Title7',
+                             ['Ingredientd', 'Ingrediente']],
+                            ['7', 'Title8', 'Instructions for Title8',
+                             ['Ingredientf', 'Ingredientg', 'Ingredient3']],
+                            ['8', 'Title9', 'Instructions for Title9',
+                             ['Ingredienth', 'Ingredienti', 'Ingredient7', 'Ingredient1']],
+                             ['9', 'Title10', 'Instructions for Title10',
                               ['Ingredient4', 'Ingredient8', 'Ingrediente']]]
         test_recipes = get_data("test_data.csv", "Data")
         self.assertEqual(find_recipes(test_recipes, [], []),
@@ -301,8 +347,8 @@ class TestCommandLine(unittest.TestCase):
         expected_output = "Usage: python cl.py --search --include_ingredients <ingredients> " \
             "--omit_ingredients <ingredients>\n<ingredients> should be a comma-separated list of " \
             "ingredients enclosed in quotes.\nor python cl.py --random <number>\nor python cl.py " \
-            "--help\n--search or --s: Search for a specific recipe.\n--random or --r: Get a random " \
-            "recipe.\n--help or --h: Display help message."
+            "--help\n--search or --s: Search for a specific recipe.\n--random or --r: Get a " \
+            "random recipe.\n--help or --h: Display help message."
 
         output = sys.stdout.getvalue().strip()
         self.assertEqual(output, expected_output, "Should be the same")
@@ -365,8 +411,6 @@ class TestMainFunction(unittest.TestCase):
         sys.stdout = StringIO()
 
         main()
-
-        self.maxDiff = None
 
         expected_recipes = "Searching for recipes...\n" \
             "['1', 'Title2', 'Instructions for Title2', ['Ingredient3', 'Ingredient4']]\n" \
