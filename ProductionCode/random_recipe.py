f8234482a367a557f4ad7b8874fd17207dd26729
Marc Eidelhoch <meidelhoch@gmail.com>
2025-04-21 19:50:36 -0500

    more formatting

--- ProductionCode/random_recipe.py
+++ ProductionCode/random_recipe.py
@@ -1,5 +1,7 @@
+'''
+This module contains a function to get a random recipe from a list of recipes.
+'''
 import random
-# from data import get_data
 
 def get_random_recipes(recipes, num_recipes):
     """
@@ -7,11 +9,3 @@ def get_random_recipes(recipes, num_recipes):
     """
 
     return random.sample(recipes, num_recipes)
-
-# if __name__ == "__main__":
-#     # Example usage
-#     random.seed(32719)  # Set a seed for reproducibility
-
-#     a = get_data("test_data.csv", "Data")
-#     random_recipes = get_random_recipes(a, 3)
-#     print(random_recipes)
