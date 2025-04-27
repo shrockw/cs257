9d0f99e7403b4e73964cfe040ca9d4580aae9288
Marc Eidelhoch <meidelhoch@gmail.com>
2025-04-27 17:58:58 -0500

    check coverage

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -15,38 +15,38 @@ class TestFlaskRoutes(unittest.TestCase):
         enter the word random, then a /, \
         then a number between 1 and 10. \
         This will return that many random recipes from the dataset. \
-        For example: /random/3 will return 3 random recipes.", response.data)
+        For example: /random/3 will return 3 random recipes.", response.data, "Should match")
 
     def test_random_route(self):
         '''Test the random route.'''
         random.seed(32719)
         response = self.client.get('/random/1')
         self.assertIn(
-        b"Broccoli with Orecchiette: With food processor running, drop in garlic and finely chop.",
-        response.data)
+        b"Sweet Ricotta Pastries",
+        response.data, "Should match")
 
     def test_random_route_invalid(self):
         '''Test the random route with an invalid number.'''
         response = self.client.get('/random/11')
-        self.assertIn(b"Please enter a number between 1 and 10.", response.data)
+        self.assertIn(b"Please enter a number between 1 and 10.", response.data, "Should match")
 
     def test_search_include_route(self):
         '''Test the search include route.'''
         response = self.client.get('/search/include/cheese,broccoli')
         self.assertIn(
-        b"Recipes including ['cheese', 'broccoli']: <br><br>",
-        response.data)
+        b"Cavatappi with Broccolini, Brown Butter, and Sage",
+        response.data, "Should match")
 
     def test_search_omit_route(self):
         '''Test the search omit route.'''
         response = self.client.get('/search/omit/cheese,broccoli')
         self.assertIn(
-        b"Recipes omitting ['cheese', 'broccoli']: <br><br>",
-        response.data)
+        b"Crispy Salt and Pepper Potatoes:",
+        response.data, "Should match")
 
     def test_search_include_omit_route(self):
         '''Test the search include and omit route.'''
         response = self.client.get('/search/include/cheese,broccoli/omit/garlic')
         self.assertIn(
-        b"Recipes including ['cheese', 'broccoli'] and omitting ['garlic']: <br><br>",
-        response.data)
+        b"Charred Steak and Broccolini with Cheese Sauce",
+        response.data, "Should match")
