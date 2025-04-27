8af271b1783e0df5a61b10ef3ccaf81052dd42a3
Willan Shrock <willanshrock@Willans-MacBook-Air-3.local>
2025-04-27 17:30:34 -0500

    cleaned error messages and style updates

--- app.py
+++ app.py
@@ -72,17 +72,22 @@ def search_include_omit(include_ingredients, omit_ingredients):
     output = ""
     for recipe in recipes:
         output += f"{recipe[1]}: {recipe[2]}<br><br>"
-    return f"Recipes including {include_ingredients} and omitting {omit_ingredients}:<br><br> {output}"
+    return f"Recipes including {include_ingredients} and omitting {omit_ingredients}: \
+        <br><br> {output}"
 
 @app.errorhandler(404)
 def page_not_found(e):
     '''This function handles 404 errors, which are page not found errors.'''
-    return f"Sorry, wrong format. Do this instead: the_url/random/number_of_recipes {str(e)}"
+    return f"Sorry, wrong format. Do this instead: the_url/random/number_of_recipes or <br><br> \
+        the_url/search/include/[ingredients separated by commas] or <br><br> \
+        the_url/search/omit/[ingredients separated by commas] or <br><br> \
+        the_url/search/[ingredients separated by commas]/omit/[ingredients separated by commas] \
+            {str(e)}"
 
 @app.errorhandler(500)
 def python_bug(e):
     '''This function handles 500 errors, which are internal server errors.'''
-    return f"Eek, a bug! {str(e)}"
+    return f"A bug! {str(e)}"
 
 if __name__ == '__main__':
     app.run()
